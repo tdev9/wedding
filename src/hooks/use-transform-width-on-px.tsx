8437533457e2476a189b5174ba264d1cbe44de38
tdev <tdevvv9@gmail.com>
2022-04-10 09:06:56 +0200

    Remove console.log

--- src/hooks/use-transform-width-on-px.tsx
+++ src/hooks/use-transform-width-on-px.tsx
@@ -1,25 +1,33 @@
 import React, { useEffect } from "react";
-export const useTransformWidthOnPx = (elementSelector: string, scrollPx: number, fadeTime: number, delay: number, maxWidth: string) => {
+export const useTransformWidthOnPx = (
+  elementSelector: string,
+  scrollPx: number,
+  fadeTime: number,
+  delay: number,
+  maxWidth: string
+) => {
   useEffect(() => {
-    const element = document.querySelector(elementSelector)!
+    const element = document.querySelector(elementSelector)!;
     let faded = false;
 
     const onScroll = function () {
-    const body = document.querySelector('html')!;
+      const body = document.querySelector("html")!;
       const y = body.scrollTop;
-      console.log(y);
       if (!faded && y > scrollPx) {
         transformWidth(element, fadeTime, delay, maxWidth);
         faded = true;
-      } else {
-        // $('.bottomMenu').fadeOut();
       }
     };
     document.addEventListener("scroll", onScroll);
   }, []);
 };
 
-function transformWidth(el: any, time: number, delay: number, maxWidth: string) {
+function transformWidth(
+  el: any,
+  time: number,
+  delay: number,
+  maxWidth: string
+) {
   el.style.opacity = 0;
 
   var last = +new Date();
@@ -27,10 +35,9 @@ function transformWidth(el: any, time: number, delay: number, maxWidth: string) 
     el.style.opacity = +el.style.opacity + (+new Date() - last) / time;
 
     last = +new Date();
-    el.style.width = maxWidth
+    el.style.width = maxWidth;
     if (+el.style.opacity < 1) {
-      (requestAnimationFrame(tick)) ||
-        setTimeout(tick, 16);
+      requestAnimationFrame(tick) || setTimeout(tick, 16);
     }
   };
 
